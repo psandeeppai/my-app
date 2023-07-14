c84b2ac6d2af913953ff8bf923cdf73c14291727
psandeeppai <psandeeppai692@gmail.com>
2023-07-14 13:25:39 +0530

    added react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import {BrowserRouter,Routes,Route} from "react-router-dom";
+// import Home from "./components/Home"
+// import About from "./components/About"
+// import Contact from "./components/Contact"
+// import NavBar from "./components/NavBar"
+// // import { Dropdown} from 'bootstrap';
+// import Signup from "./components/Signup"
+// import Login from "./components/Login"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+  return(
+    // <div>
+    //   <h1>Today is react Class</h1>
+
+    // </div>
+    //fragment method
+    // <>
+    // <h1 className='bg-info'>Today is react class</h1>
+    // <p className='bg-danger'>this is a paragraph</p>
+    // </>
+    <>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/About" element={<About/>}/>
+      <Route path="/Contact" element={<Contact/>}/>
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Signup" element={<Signup/>}/>
+      
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+
+export default App
+
